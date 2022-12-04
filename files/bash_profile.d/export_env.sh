12895e61c42f0602bfa644cb99e76eab84522950
nakahiro386 <nakahiro386@gmail.com>
2022-12-05 00:56:15 +0900

    環境変数にVIRTUALENV_ALWAYS_COPY=1を追加

--- files/bash_profile.d/export_env.sh
+++ files/bash_profile.d/export_env.sh
@@ -3,5 +3,6 @@ export HISTSIZE=5000
 export HISTCONTROL=ignoreboth:erasedups
 export HISTIGNORE=?:??:???:exit:
 export PIPENV_VENV_IN_PROJECT=true
+export VIRTUALENV_ALWAYS_COPY=1
 export PYTHON_CONFIGURE_OPTS="--enable-shared"
 export RUBY_CONFIGURE_OPTS="--enable-shared"

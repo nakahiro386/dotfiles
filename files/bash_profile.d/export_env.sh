145bf4dbb790ac4f9c61d6b980bc5370a53bb9ba
nakahiro386 <nakahiro386@gmail.com>
2022-12-31 00:37:16 +0900

    git commit gpgsign = true

--- files/bash_profile.d/export_env.sh
+++ files/bash_profile.d/export_env.sh
@@ -6,3 +6,4 @@ export PIPENV_VENV_IN_PROJECT=true
 export VIRTUALENV_ALWAYS_COPY=1
 export PYTHON_CONFIGURE_OPTS="--enable-shared"
 export RUBY_CONFIGURE_OPTS="--enable-shared"
+export GPG_TTY=$(tty)

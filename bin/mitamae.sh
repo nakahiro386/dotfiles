2cd533bb512cef39c70296d760a6f23745d39b52
nakahiro386 <nakahiro386@gmail.com>
2024-05-11 14:35:47 +0900

    mitamaeのファイルハッシュチェックにsha256ファイルを使用しないよう変更

--- bin/mitamae.sh
+++ bin/mitamae.sh
@@ -5,15 +5,13 @@ bin_dir=$(cd "$(dirname $0)" && pwd)
 mitamae_version='1.14.1'
 mitamae_bin="${bin_dir}/mitamae-x86_64-linux-${mitamae_version}"
 mitamae_sha256='ce5d10e2fde981707067da454aa845df02bdbcfba1ea2cba46b0ff1412f20016'
-mitamae_sha256_file="${bin_dir}/mitamae.sha256"
 
 if [ ! -e "${mitamae_bin}" ]; then
     wget -q -O "${mitamae_bin}" https://github.com/itamae-kitchen/mitamae/releases/download/v${mitamae_version}/mitamae-x86_64-linux
     chmod +x "${mitamae_bin}"
-    echo "${mitamae_sha256} *${mitamae_bin}" > ${mitamae_sha256_file}
 fi
 
-if ! sha256sum --quiet --status -c ${mitamae_sha256_file}; then
+if ! echo "${mitamae_sha256} *${mitamae_bin}" | sha256sum --quiet --status -c; then
     echo "checksum verificatoin failed!"
     exit 1
 fi
